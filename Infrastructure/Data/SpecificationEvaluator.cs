6e4d056fe0465d0fd1f7d2c23eee548661a8ea22
Abdallah <abdohfox17@gmail.com>
2021-02-13 23:55:03 +0200

    implements pagination , filtering and sorting

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -12,6 +12,19 @@ namespace Infrastructure.Data {
                 query = query.Where(spec.Criteria);
             }
 
+            // add orderby to query 
+            if (spec.OrderBy != null) {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDesc != null) {
+                query = query.OrderByDescending(spec.OrderByDesc);
+            }
+
+            if (spec.IsPagingEnabled) {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             return query;
